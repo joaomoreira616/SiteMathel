e09ed4b0fafaacb4cda9ed5d8027052b920b53e5
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2024-08-28 11:33:30 -0300

    códogos iniciais

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+// JavaScript para possíveis futuras funcionalidades interativas
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site Mathel Hidráulica e Elétrica carregado.");
+});
